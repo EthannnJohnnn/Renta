acc404907e26986ae65ec69ec8291813c507145c
Ethan <ethanjohn.montejo@cit.edu>
2026-05-20 20:14:14 +0800

    added image in property

--- src/main/java/models/Property.java
+++ src/main/java/models/Property.java
@@ -6,6 +6,7 @@ public class Property {
     private String name;
     private String address;
     private String description;
+    private String imageUrl;
 
     public Property() {}
 
@@ -40,7 +41,12 @@ public class Property {
     public String getDescription() { return description; }
     public void setDescription(String description) { this.description = description; }
 
+<<<<<<< HEAD
     public String getImageUrl() {
         return null; //temporary
     }
+=======
+    public String getImageUrl() { return imageUrl; }
+    public void setImageUrl(String imageUrl) { this.imageUrl = imageUrl; }
+>>>>>>> 55eafe1 (added image in property)
 }
